33222b9c88c5ad9fb082f529bd5e966a83e96c2d
Santasila Bryan <113746524+SBryanK@users.noreply.github.com>
2024-10-18 16:58:07 +0800

    Create pipe(etl).py
    Data Pipeline example of ETL, more to come

---
+++ Data Pipeline/pipe(etl).py
@@ -0,0 +1,32 @@
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+
+#Exstract
+def extract()-> dict:
+    API_URL = "http://universities.hippolabs.com/search?country=United+States"
+    data = requests.get(API_URL).json()
+    return data
+
+# Transform
+def transform(data:dict) -> pd.DataFrame:
+    df = pd.DataFrame(data)
+    print(f"Total Number of universities from API {len(data)}")
+
+    df = df[df["name"].str.contains("California")]
+    print(f"Number of universities in california {len(df)}")
+    
+    df['domains'] = [','.join(map(str, l)) for l in df['domains']]
+    df['web_pages'] = [','.join(map(str, l)) for l in df['web_pages']]
+    df = df.reset_index(drop=True)
+    
+    return df[["domains","country","web_pages","name"]]
+
+# Load
+def load(df:pd.DataFrame):
+    engine = create_engine('sqlite:///lite_store.db')
+    df.to_sql('cal_uni', engine, if_exists='replace')
+
+data = extract()
+df = transform(data)
+load(df)
